18d359ddc8898a9ee3321e8b560a3c5adfbf1994
William Koerich <williamkoerich17@gmail.com>
2023-03-16 16:27:58 -0300

    feat: created validation exception class

---
+++ core/classes/ValidationException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Validation Exception
+ *
+ */
+
+class ValidationException extends Exception
+{
+	public $errors;
+
+	function __construct( $errors )
+	{
+		$this->errors = $errors; 
+	}
+
+	public function getError()
+	{
+		return $this->errors;
+	}
+}
