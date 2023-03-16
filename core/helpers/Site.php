4d7fab6b5c05b75bce14132c6a6fe677b7e400b3
William Koerich <williamkoerich17@gmail.com>
2023-03-16 16:33:00 -0300

    feat: created site helper

---
+++ core/helpers/Site.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Site
+ *
+ */
+
+class Site
+{
+
+	/**
+	 * Pagination
+	 */
+	public static function pagination( array $pagination ) : void
+	{
+		require( ROOT . '/app/views/pagination.php' );
+	}
+
+	/**
+	 * Redirect
+	 */
+	public static function redirect( string $location ) : void
+	{
+		header( 'Location: ' . $location );
+	}
+
+	/**
+	 * Title
+	 */
+	public static function title( $title, string $separator = '#' ) : string
+	{
+		$name = 'CRUD MVC OOP PDO';
+
+		if ( ! empty( $title ) ) {
+			$title = $title . ' ' . $separator . ' ' . $name;
+		} else {
+			$title = $name;
+		}
+
+		return $title;
+	}
+
+	/**
+	 * Version
+	 */
+	public static function version() : string
+	{
+		$version = '0.0.0';
+
+		if ( file_exists( 'version.json' ) ) {
+			$json    = file_get_contents( 'version.json' );
+			$obj     = json_decode( $json );
+			$version = $obj->version;
+		}
+
+		return $version;
+	}
+
+}
